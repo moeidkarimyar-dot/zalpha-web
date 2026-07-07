144f17e161e45fa08e951d9eed752fff9a14ea0d
Moeid Karimyar <moeidkarimyar@gmail.com>
2026-07-07 22:54:57 +0330

    test code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
-console.log("Hello, World!");
-let userName = prompt("Please enter your name:");
-console.log(userName);
+// console.log("Hello, World!");
+// let userName = prompt("Please enter your name:");
+// console.log(userName);
+
+let Age = 25;
+
+console.log(typeof Age);
+console.log(Age);
+
+let stringeAge = String(Age);
+
+console.log(typeof stringeAge);
+console.log(stringeAge);
